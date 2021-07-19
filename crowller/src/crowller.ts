f690b214f44dbed146bf372222ae551c29f4117e
wcs <1103409364@qq.com>
2021-07-19 23:28:05 +0800

    get img src
    Signed-off-by: wcs <1103409364@qq.com>

--- crowller/src/crowller.ts
+++ crowller/src/crowller.ts
@@ -1,10 +1,10 @@
 // ts 直接引用js类库会报错，需要类型定义文件。鼠标移到对应的位置，按提示安装类型定义文件@types/superagent
 // ts -> .d.ts类型声明文件 -> js
 import superagent from 'superagent';
+import cheerio from 'cheerio';
 
 class Crowller {
   private url = 'https://baidu.com';
-  private rawHtml = '';
 
   constructor() {
     this.getRawHtml();
@@ -12,7 +12,13 @@ class Crowller {
 
   async getRawHtml() {
     const res = await superagent.get(this.url);
-    this.rawHtml = res.text;
+    this.getInfo(res.text);
+  }
+
+  getInfo(rawHtml: string) {
+    const $ = cheerio.load(rawHtml);
+    const img = $('#s_lg_img');
+    console.log(img.attr('src'));
   }
 }
 
