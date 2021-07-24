6af77fcd7fda3f7c9b604087f660a17db64f1f37
wcs <1103409364@qq.com>
2021-07-24 13:36:56 +0800

    typeProtection
    Signed-off-by: wcs <1103409364@qq.com>

---
+++ typeProtection/demo.ts
@@ -0,0 +1,49 @@
+interface Bird {
+  fly: boolean;
+  sing: () => {};
+}
+
+interface Dog {
+  fly: boolean;
+  bark: () => {};
+}
+// 联合类型（A|B 竖线分隔） 类型保护
+function trainAnimal(animal: Bird | Dog) {
+  // animal.sing(); // 报错，ts 不知道是哪种类型
+  // if (animal.fly) {
+  //   animal.sing(); //报错，ts 不知道有这个方法
+  // }
+
+  // 1通过断言的方式做类型保护
+  if (animal.fly) {
+    (animal as Bird).sing();
+  } else {
+    (animal as Dog).bark();
+  }
+}
+// 2通过 in 语法做类型保护
+function trainAnimal2(animal: Bird | Dog) {
+  if ("sing" in animal) {
+    animal.sing();
+  } else {
+    animal.bark();
+  }
+}
+//
+function add(a: string | number, b: string | number) {
+  if (typeof a === "string" || typeof b === "string") {
+    return `${a} ${b}`;
+  }
+  return a + b;
+}
+
+class NumberObj {
+  count: number;
+}
+// NumberObj 不能是interface，因为它不支持instanceof
+function addSecond(a: object | NumberObj, b: number | NumberObj) {
+  if (a instanceof NumberObj && b instanceof NumberObj) {
+    return a.count + b.count;
+  }
+  return 0;
+}
