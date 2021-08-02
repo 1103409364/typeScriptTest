a1090cb137acc8efbd376764b52ac2e3f12e8258
wcs <1103409364@qq.com>
2021-08-02 22:56:16 +0800

    类型描述不准确问题、类型扩展问题
    Signed-off-by: wcs <1103409364@qq.com>

--- crowllerServer/src/router.ts
+++ crowllerServer/src/router.ts
@@ -4,16 +4,38 @@ import Analyzer1 from './analyzer';
 
 const router = Router();
 
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined; // 泛匹配
+  };
+}
+
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+  res.send(`
+  <html>
+    <body>
+      <form method="post" action="/getData">
+        <input type="password" name="password" />
+        <button>提交</button>
+      </form>
+    </body>
+  </html>
+  `);
 });
 
-router.get('/getData', (req: Request, res: Response) => {
-  const url = 'https://baidu.com';
-  const analyzer = Analyzer1.getInstance();
-  new Crowller(analyzer, url);
+router.post('/getData', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body; // body undefined，需要解析中间件body-parser。ts没有错误提示 express类型声明文件中req是any类型
+  // 这里的 password 是any类型，改d.ts没有意义
 
-  res.send('getData success');
+  if (password === '123') {
+    const url = 'https://baidu.com';
+    const analyzer = Analyzer1.getInstance();
+    new Crowller(analyzer, url);
+
+    res.send('getData success');
+  } else {
+    res.send(req.test + 'password Error!');
+  }
 });
 
 export default router;
